d3b7c83c431b10be8abbc45fb89885ccd091e151
Selma Copra <selma.copra@codescene.com>
2024-09-23 09:09:00 +0200

    DE-28: Add server URL constant to global settings

--- src/main/kotlin/com/codescene/jetbrains/config/global/CodeSceneGlobalSettings.kt
+++ src/main/kotlin/com/codescene/jetbrains/config/global/CodeSceneGlobalSettings.kt
@@ -1,12 +1,13 @@
 package com.codescene.jetbrains.config.global
 
+import com.codescene.jetbrains.util.Constants.CODESCENE_SERVER_URL
 import org.jetbrains.annotations.NonNls
 
 data class CodeSceneGlobalSettings(
     @NonNls var foldingRangeProvider: String = "",
     @NonNls var defaultEditorFormatter: String = "",
     @NonNls var defaultNotebookFormatter: String = "",
-    @NonNls var serverUrl: String = "https://codescene.io",
+    @NonNls var serverUrl: String = CODESCENE_SERVER_URL,
 
     var enableCodeLenses: Boolean = true,
     var enableAutoRefactor: Boolean = false,
