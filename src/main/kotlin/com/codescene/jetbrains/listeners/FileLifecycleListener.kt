ab6d8a5dcf44bce67dd7098220f592b697716482
Selma Copra <selma.copra@codescene.com>
2024-11-06 14:07:18 +0100

    DE-12: add Git4Idea bundled plugin

--- src/main/kotlin/com/codescene/jetbrains/listeners/FileLifecycleListener.kt
+++ src/main/kotlin/com/codescene/jetbrains/listeners/FileLifecycleListener.kt
@@ -4,11 +4,23 @@ import com.codescene.jetbrains.services.CodeSceneService
 import com.intellij.openapi.fileEditor.FileEditorManager
 import com.intellij.openapi.fileEditor.FileEditorManagerListener
 import com.intellij.openapi.vfs.VirtualFile
+import git4idea.repo.GitRepositoryManager
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class FileLifecycleListener : FileEditorManagerListener {
     override fun fileClosed(source: FileEditorManager, file: VirtualFile) {
         val codeSceneService = CodeSceneService.getInstance(source.project)
 
         codeSceneService.cancelFileReview(file.path)
+    }
+
+    override fun fileOpened(source: FileEditorManager, file: VirtualFile) {
+        CoroutineScope(Dispatchers.IO).launch {
+            val gitRepository = GitRepositoryManager.getInstance(source.project).getRepositoryForFile(file)
+
+            println("GitRepositoryInfo ${gitRepository?.info}")
+        }
     }
 }
