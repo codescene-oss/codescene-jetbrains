aebfcbd97fd4433ae39900a349ee51643323d5af
Selma Copra <selma.copra@codescene.com>
2024-11-06 16:24:01 +0100

    DE-12: add logic to read from HEAD commit

--- src/main/kotlin/com/codescene/jetbrains/listeners/FileLifecycleListener.kt
+++ src/main/kotlin/com/codescene/jetbrains/listeners/FileLifecycleListener.kt
@@ -4,6 +4,9 @@ import com.codescene.jetbrains.services.CodeSceneService
 import com.intellij.openapi.fileEditor.FileEditorManager
 import com.intellij.openapi.fileEditor.FileEditorManagerListener
 import com.intellij.openapi.vfs.VirtualFile
+import git4idea.commands.Git
+import git4idea.commands.GitCommand
+import git4idea.commands.GitLineHandler
 import git4idea.repo.GitRepositoryManager
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -18,9 +21,22 @@ class FileLifecycleListener : FileEditorManagerListener {
 
     override fun fileOpened(source: FileEditorManager, file: VirtualFile) {
         CoroutineScope(Dispatchers.IO).launch {
-            val gitRepository = GitRepositoryManager.getInstance(source.project).getRepositoryForFile(file)
+            val gitRepository =
+                GitRepositoryManager.getInstance(source.project).getRepositoryForFile(file) ?: return@launch
+
+            val handler = GitLineHandler(source.project, gitRepository.root, GitCommand.SHOW)
+
+            handler.addParameters("HEAD:${file.path.substringAfter(gitRepository.root.path + "/")}")
 
             println("GitRepositoryInfo ${gitRepository?.info}")
+
+            try {
+                val output = Git.getInstance().runCommand(handler).output
+
+                println("Output: ${output.joinToString("\n")}")
+            } catch (e: Exception) {
+                println(e.message)
+            }
         }
     }
 }
