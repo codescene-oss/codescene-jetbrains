80032893dd4ed255cc00036528e76ac892d9ab51
Selma Copra <selma.copra@codescene.com>
2025-01-14 16:06:50 +0100

    CS-2283: remove unimplemneted features/links

--- src/main/kotlin/com/codescene/jetbrains/components/settings/tab/GeneralTab.kt
+++ src/main/kotlin/com/codescene/jetbrains/components/settings/tab/GeneralTab.kt
@@ -2,7 +2,6 @@ package com.codescene.jetbrains.components.settings.tab
 
 import com.codescene.jetbrains.CodeSceneIcons.STATUS
 import com.codescene.jetbrains.UiLabelsBundle
-import com.codescene.jetbrains.util.Constants.AI_PRINCIPLES_URL
 import com.codescene.jetbrains.util.Constants.CONTACT_URL
 import com.codescene.jetbrains.util.Constants.DOCUMENTATION_URL
 import com.codescene.jetbrains.util.Constants.SUPPORT_URL
@@ -11,7 +10,6 @@ import com.codescene.jetbrains.util.Log
 import com.intellij.icons.AllIcons
 import com.intellij.openapi.options.Configurable
 import com.intellij.ui.JBColor
-import com.intellij.ui.RoundedLineBorder
 import com.intellij.util.IconUtil
 import com.intellij.util.ui.JBUI
 import java.awt.*
@@ -27,100 +25,50 @@ class GeneralTab : Configurable {
     private val usersIcon = IconUtil.colorize(AllIcons.General.User, JBUI.CurrentTheme.IconBadge.INFORMATION)
     private val statusIcon = IconUtil.colorize(STATUS, JBUI.CurrentTheme.IconBadge.INFORMATION)
 
-    private val account = listOf(
-        UiLabelsBundle.message("upgradeToPro") to "", //TBD
-        UiLabelsBundle.message("signIn") to "" //TBD
-    )
-
-    private val status = listOf(
-        UiLabelsBundle.message("ace") to "", //TBD
-        UiLabelsBundle.message("codeHealthAnalysis") to "" //TBD
-    )
-
     private val more = listOf(
         UiLabelsBundle.message("documentation") to DOCUMENTATION_URL,
         UiLabelsBundle.message("termsAndPolicies") to TERMS_AND_CONDITIONS_URL,
-        UiLabelsBundle.message("aiPrinciples") to AI_PRINCIPLES_URL,
+//        UiLabelsBundle.message("aiPrinciples") to AI_PRINCIPLES_URL, TODO: uncomment when ACE capabilities are added
         UiLabelsBundle.message("contactCodeScene") to CONTACT_URL,
         UiLabelsBundle.message("supportTicket") to SUPPORT_URL
     )
 
     override fun createComponent() = JPanel().apply {
+        layout = BorderLayout()
+
+        add(getMoreSection(), BorderLayout.NORTH)
+    }
+
+    private fun getMoreSection() = JPanel().apply {
         layout = BoxLayout(this, BoxLayout.Y_AXIS)
 
+        val message = UiLabelsBundle.message("more")
+        val icon = IconUtil.scale(globeIcon, null, 1.2f)
+
+        add(Box.createVerticalStrut(10))
+        addHeader(message, icon)
         add(Box.createVerticalStrut(10))
 
-        add(JPanel().apply {
+        more.map {
+            add(createRoundedPanel(it.first, it.second))
+            add(Box.createVerticalStrut(10))
+        }
+    }
+
+    private fun JPanel.addHeader(message: String, icon: Icon) {
+        val panel = JPanel().apply {
             layout = FlowLayout(FlowLayout.LEFT, 5, 0)
 
-            val statusIcon = JLabel(usersIcon)
-            val status = JLabel(UiLabelsBundle.message("account")).apply {
+            val statusIcon = JLabel(icon)
+            val status = JLabel(message).apply {
                 font = Font("Arial", Font.BOLD, 13)
             }
 
             add(statusIcon)
             add(status)
-        })
-
-        add(Box.createVerticalStrut(10))
-
-        account.map {
-            add(createRoundedPanel(it.first, it.second, false))
-            add(Box.createVerticalStrut(10))
         }
 
-        add(Box.createVerticalStrut(10))
-
-        add(JPanel().apply {
-            layout = BoxLayout(this, BoxLayout.Y_AXIS)
-
-            add(Box.createVerticalGlue())
-
-            add(JPanel().apply {
-                layout = FlowLayout(FlowLayout.LEFT, 5, 0)
-
-                val statusIcon = JLabel(statusIcon)
-                val status = JLabel(UiLabelsBundle.message("status")).apply {
-                    font = Font("Arial", Font.BOLD, 13)
-                }
-
-                add(statusIcon)
-                add(status)
-            })
-
-            add(Box.createVerticalStrut(10))
-
-            status.map {
-                add(createRoundedPanel(it.first, it.second))
-                add(Box.createVerticalStrut(10))
-            }
-            add(Box.createVerticalStrut(10))
-        })
-
-        add(JPanel().apply {
-            layout = BoxLayout(this, BoxLayout.Y_AXIS)
-
-            add(Box.createVerticalGlue())
-
-            add(JPanel().apply {
-                layout = FlowLayout(FlowLayout.LEFT, 5, 0)
-
-                val statusIcon = JLabel(IconUtil.scale(globeIcon, null, 1.2f))
-                val status = JLabel(UiLabelsBundle.message("more")).apply {
-                    font = Font("Arial", Font.BOLD, 13)
-                }
-
-                add(statusIcon)
-                add(status)
-            })
-
-            add(Box.createVerticalStrut(10))
-
-            more.map {
-                add(createRoundedPanel(it.first, it.second))
-                add(Box.createVerticalStrut(10))
-            }
-        })
+        add(panel)
     }
 
     override fun isModified(): Boolean = false
@@ -143,7 +91,7 @@ class GeneralTab : Configurable {
         override fun getBorderInsets(c: Component?) = JBUI.insets(0, 10)
     }
 
-    private fun createRoundedPanel(text: String, link: String, isLink: Boolean = true): JPanel {
+    private fun createRoundedPanel(text: String, link: String): JPanel {
         val padding = 3
         val maxHeight = 35
 
@@ -158,42 +106,31 @@ class GeneralTab : Configurable {
                 horizontalAlignment = SwingConstants.LEFT
             }, BorderLayout.WEST)
 
-            if (isLink) {
-                add(JLabel(AllIcons.General.ChevronRight).apply {
-                    horizontalAlignment = SwingConstants.RIGHT
-                }, BorderLayout.EAST)
+            add(JLabel(AllIcons.General.ChevronRight).apply {
+                horizontalAlignment = SwingConstants.RIGHT
+            }, BorderLayout.EAST)
 
-                addMouseListener(object : MouseAdapter() {
-                    override fun mouseClicked(e: MouseEvent?) {
-                        try {
-                            val uri = URI(link)
+            addMouseListener(createLinkMouseListener(link))
+        }
+    }
 
-                            if (Desktop.isDesktopSupported()) Desktop.getDesktop().browse(uri)
-                        } catch (e: Exception) {
-                            Log.warn("Unable to open link: ${e.message}")
-                        }
-                    }
+    private fun createLinkMouseListener(link: String): MouseAdapter {
+        return object : MouseAdapter() {
+            override fun mouseClicked(e: MouseEvent?) {
+                try {
+                    val uri = URI(link)
+                    if (Desktop.isDesktopSupported()) Desktop.getDesktop().browse(uri)
+                } catch (e: Exception) {
+                    Log.warn("Unable to open link: ${e.message}")
+                }
+            }
 
-                    override fun mouseEntered(e: MouseEvent?) {
-                        cursor = Cursor.getPredefinedCursor(Cursor.HAND_CURSOR)
-                    }
+            override fun mouseEntered(e: MouseEvent?) {
+                (e?.component as? JComponent)?.cursor = Cursor.getPredefinedCursor(Cursor.HAND_CURSOR)
+            }
 
-                    override fun mouseExited(e: MouseEvent?) {
-                        cursor = Cursor.getDefaultCursor()
-                    }
-                })
-            } else {
-                add(JButton("COMING SOON").apply {
-                    foreground = JBColor.BLACK
-                    isContentAreaFilled = false
-                    isFocusPainted = false
-                    font = Font("Arial", Font.BOLD, 10)
-                    border = RoundedLineBorder(JBColor.BLACK, 12)
-                    isOpaque = false
-                    preferredSize = Dimension(100, 5)
-                    minimumSize = Dimension(100, 100)
-                    maximumSize = Dimension(100, 100)
-                }, BorderLayout.EAST)
+            override fun mouseExited(e: MouseEvent?) {
+                (e?.component as? JComponent)?.cursor = Cursor.getDefaultCursor()
             }
         }
     }
