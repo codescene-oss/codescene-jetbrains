b9b14563aa53823153ba0df18ad4a4dce245e4b8
Selma Copra <selma.copra@codescene.com>
2025-01-30 17:13:59 +0100

    CS-4181: add telemetry consent notification

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,11 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.1.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
 ## [Unreleased]
+### Added
+- Telemetry consent prompt and state (false by default).
+
+### Changed
+- Telemetry events will only be logged once user consent is given.
 
 ## [0.0.9-beta] - 2024-01-29
 ### Added
---
+++ src/main/kotlin/com/codescene/jetbrains/util/NotificationUtils.kt
@@ -0,0 +1,32 @@
+package com.codescene.jetbrains.util
+
+import com.codescene.jetbrains.UiLabelsBundle
+import com.codescene.jetbrains.config.global.CodeSceneGlobalSettingsStore
+import com.codescene.jetbrains.util.Constants.CODESCENE
+import com.intellij.notification.NotificationGroupManager
+import com.intellij.notification.NotificationType
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.project.Project
+
+fun showTelemetryConsentNotification(project: Project?) {
+    val notification = NotificationGroupManager.getInstance()
+        .getNotificationGroup(CODESCENE)
+        .createNotification(CODESCENE, UiLabelsBundle.message("telemetryDescription"), NotificationType.INFORMATION)
+
+    notification.addAction(object : AnAction(UiLabelsBundle.message("acceptButton")) {
+        override fun actionPerformed(e: AnActionEvent) {
+            val state = CodeSceneGlobalSettingsStore.getInstance()
+            state.updateTelemetryConsent(true)
+            notification.expire()
+        }
+    })
+
+    notification.addAction(object : AnAction(UiLabelsBundle.message("ignoreButton")) {
+        override fun actionPerformed(e: AnActionEvent) {
+            notification.expire()
+        }
+    })
+
+    notification.notify(project)
+}
