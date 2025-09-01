5a4466ceff9a7e52c9f595366bc0900f4f4ab18b
Selma Copra <selma.copra@codescene.com>
2025-09-01 15:06:57 +0200

    Enable monitor flag

--- src/main/kotlin/com/codescene/jetbrains/config/global/CodeSceneGlobalSettings.kt
+++ src/main/kotlin/com/codescene/jetbrains/config/global/CodeSceneGlobalSettings.kt
@@ -26,7 +26,7 @@ data class CodeSceneGlobalSettings(
     var excludeGitignoreFiles: Boolean = true,
     var previewCodeHealthGate: Boolean = false,
     var telemetryConsentGiven: Boolean = false,
-    val codeHealthMonitorEnabled: Boolean = false, //Freemium version
+    val codeHealthMonitorEnabled: Boolean = true, //Freemium version
     var monitorTreeSortOption: MonitorTreeSortOptions = MonitorTreeSortOptions.SCORE_ASCENDING
 ) {
     var aceStatus: AceStatus by aceStatusDelegate()
