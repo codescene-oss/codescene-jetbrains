547258a42a0aea522b78a7304a5248284900c61d
Selma Copra <selma.copra@codescene.com>
2025-09-26 15:05:26 +0200

    Add check if function is stale in ACE view

--- src/main/kotlin/com/codescene/jetbrains/fileeditor/ace/CwfAceFileEditorProvider.kt
+++ src/main/kotlin/com/codescene/jetbrains/fileeditor/ace/CwfAceFileEditorProvider.kt
@@ -1,5 +1,7 @@
 package com.codescene.jetbrains.fileeditor.ace
 
+import com.codescene.data.ace.FnToRefactor
+import com.codescene.data.ace.RefactorResponse
 import com.codescene.jetbrains.components.webview.data.view.AceData
 import com.intellij.openapi.fileEditor.FileEditor
 import com.intellij.openapi.fileEditor.FileEditorPolicy
@@ -9,7 +11,13 @@ import com.intellij.openapi.util.Key
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.testFramework.LightVirtualFile
 
-val CWF_ACE_DATA_KEY: Key<AceData> = Key.create("codescene.ace.data")
+data class CwfAceFileEditorProviderData(
+    val aceData: AceData?,
+    val functionToRefactor: FnToRefactor,
+    val refactorResponse: RefactorResponse?
+)
+
+val CWF_ACE_DATA_KEY: Key<CwfAceFileEditorProviderData> = Key.create("codescene.ace.data")
 
 @Suppress("UnstableApiUsage")
 internal class CwfAceFileEditorProvider : FileEditorProvider {
@@ -22,7 +30,7 @@ internal class CwfAceFileEditorProvider : FileEditorProvider {
         file is LightVirtualFile && file.getUserData(CWF_ACE_DATA_KEY) != null
 
     override fun createEditor(project: Project, file: VirtualFile): FileEditor {
-        val data = file.getUserData(CWF_ACE_DATA_KEY) ?: error("CwfData<AceData> is required to open this editor.")
+        val data = file.getUserData(CWF_ACE_DATA_KEY) ?: error("CwfData<CwfAceFileEditorProviderData> is required to open this editor.")
 
         return CwfAceFileEditor(project, file, data)
     }
