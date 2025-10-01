c870b60ae6865daf21499366339a3fe6da16db6e
Selma Copra <selma.copra@codescene.com>
2025-10-01 10:11:31 +0200

    Extract common logic, add docs

--- src/main/kotlin/com/codescene/jetbrains/fileeditor/ace/CwfAceFileEditor.kt
+++ src/main/kotlin/com/codescene/jetbrains/fileeditor/ace/CwfAceFileEditor.kt
@@ -1,54 +1,15 @@
 package com.codescene.jetbrains.fileeditor.ace
 
-import com.codescene.jetbrains.components.webview.WebViewFactory
-import com.codescene.jetbrains.components.webview.WebViewInitializer
 import com.codescene.jetbrains.components.webview.data.View
-import com.intellij.openapi.fileEditor.FileEditor
-import com.intellij.openapi.fileEditor.FileEditorState
+import com.codescene.jetbrains.components.webview.data.view.AceData
+import com.codescene.jetbrains.fileeditor.BaseCwfFileEditor
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.util.UserDataHolderBase
 import com.intellij.openapi.vfs.VirtualFile
-import java.beans.PropertyChangeListener
-import javax.swing.JComponent
 
-class CwfAceFileEditor(private val project: Project, private val file: VirtualFile, data: CwfAceFileEditorProviderData) :
-    UserDataHolderBase(), FileEditor {
-    private val component: JComponent
-
-    init {
-        val content = WebViewFactory.createWebViewComponent(
-            project = project,
-            view = View.ACE,
-            initialData = data.aceData
-        )
-        component = content.component
-    }
-
-    override fun getComponent(): JComponent = component
-
-    override fun getPreferredFocusedComponent(): JComponent = component
-
-    override fun getName(): String = file.name
-
-    override fun setState(p0: FileEditorState) {
-        // No implementation needed
-    }
-
-    override fun getFile() = file
-
-    override fun isModified() = false
-
-    override fun isValid(): Boolean = file.isValid
-
-    override fun addPropertyChangeListener(p0: PropertyChangeListener) {
-        // No implementation needed
-    }
-
-    override fun removePropertyChangeListener(p0: PropertyChangeListener) {
-        // No implementation needed
-    }
-
-    override fun dispose() {
-        WebViewInitializer.getInstance(project).unregisterBrowser(View.ACE)
-    }
-}
+class CwfAceFileEditor(project: Project, file: VirtualFile, data: CwfAceFileEditorProviderData) :
+    BaseCwfFileEditor<AceData?>(
+        project,
+        file,
+        View.ACE,
+        data.aceData
+    )
--- src/main/kotlin/com/codescene/jetbrains/fileeditor/ace/acknowledge/CwfAceAcknowledgeFileEditor.kt
+++ src/main/kotlin/com/codescene/jetbrains/fileeditor/ace/acknowledge/CwfAceAcknowledgeFileEditor.kt
@@ -1,59 +1,19 @@
 package com.codescene.jetbrains.fileeditor.ace.acknowledge
 
-import com.codescene.jetbrains.components.webview.WebViewFactory
-import com.codescene.jetbrains.components.webview.WebViewInitializer
 import com.codescene.jetbrains.components.webview.data.View
 import com.codescene.jetbrains.components.webview.data.view.AceAcknowledgeData
-import com.intellij.openapi.fileEditor.FileEditor
-import com.intellij.openapi.fileEditor.FileEditorState
+import com.codescene.jetbrains.fileeditor.BaseCwfFileEditor
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.util.UserDataHolderBase
 import com.intellij.openapi.vfs.VirtualFile
-import java.beans.PropertyChangeListener
-import javax.swing.JComponent
 
 class CwfAceAcknowledgeFileEditor(
-    private val project: Project,
-    private val file: VirtualFile,
-    data: AceAcknowledgeData
+    project: Project,
+    file: VirtualFile,
+    data: CwfAceAcknowledgeEditorProviderData
 ) :
-    UserDataHolderBase(), FileEditor {
-    private val component: JComponent
-
-    init {
-        val content = WebViewFactory.createWebViewComponent(
-            project = project,
-            view = View.ACE_ACKNOWLEDGE,
-            initialData = data
-        )
-        component = content.component
-    }
-
-    override fun getComponent(): JComponent = component
-
-    override fun getPreferredFocusedComponent(): JComponent = component
-
-    override fun getName(): String = file.name
-
-    override fun setState(p0: FileEditorState) {
-        // No implementation needed
-    }
-
-    override fun getFile() = file
-
-    override fun isModified() = false
-
-    override fun isValid(): Boolean = file.isValid
-
-    override fun addPropertyChangeListener(p0: PropertyChangeListener) {
-        // No implementation needed
-    }
-
-    override fun removePropertyChangeListener(p0: PropertyChangeListener) {
-        // No implementation needed
-    }
-
-    override fun dispose() {
-        WebViewInitializer.getInstance(project).unregisterBrowser(View.ACE_ACKNOWLEDGE)
-    }
-}
+    BaseCwfFileEditor<AceAcknowledgeData>(
+        project,
+        file,
+        View.ACE_ACKNOWLEDGE,
+        data.aceAcknowledgeData
+    )
