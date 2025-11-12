d495b6e2e6b946b49feac330c97ddfb9be90b76b
Selma Copra <selma.copra@codescene.com>
2025-11-12 09:17:24 +0100

    chore: delete native monitor files

--- src/main/kotlin/com/codescene/jetbrains/util/CodeDeltaUtils.kt
+++ src/main/kotlin/com/codescene/jetbrains/util/CodeDeltaUtils.kt
@@ -1,35 +1,12 @@
 package com.codescene.jetbrains.util
 
-import com.codescene.data.delta.ChangeDetail
-import com.codescene.data.delta.Delta
-import com.codescene.data.delta.Function
-import com.codescene.jetbrains.config.global.CodeSceneGlobalSettingsStore
-import com.codescene.jetbrains.config.global.MonitorTreeSortOptions
 import com.codescene.jetbrains.services.GitService
+import com.codescene.jetbrains.services.api.deltamodels.NativeDelta
 import com.codescene.jetbrains.services.cache.DeltaCacheQuery
 import com.codescene.jetbrains.services.cache.DeltaCacheService
 import com.intellij.openapi.editor.Editor
-import com.intellij.openapi.util.text.StringUtil.pluralize
-import java.util.*
-import java.util.concurrent.ConcurrentHashMap
 
-private fun MutableList<String>.countFixesAndDegradations(details: List<ChangeDetail>) {
-    val fixesAndDegradations = details.groupingBy { isPositiveChange(it.changeType) }.eachCount()
-
-    fixesAndDegradations[true]?.let { add("$it ${pluralize("issue", it)} fixed") }
-    fixesAndDegradations[false]?.let { add("$it ${pluralize("issue", it)} degrading code health") }
-}
-
-fun getFunctionDeltaTooltip(function: Function, details: List<ChangeDetail>): String {
-    val tooltip = mutableListOf("Function \"${function.name}\"")
-
-    tooltip.countFixesAndDegradations(details)
-    //TODO: ACE information
-
-    return tooltip.joinToString(separator = " • ")
-}
-
-fun getCachedDelta(editor: Editor): Pair<Boolean, Delta?> {
+fun getCachedDelta(editor: Editor): Pair<Boolean, NativeDelta?> {
     val project = editor.project!!
 
     val oldCode = GitService.getInstance(project).getBranchCreationCommitCode(editor.virtualFile)
@@ -37,32 +14,4 @@ fun getCachedDelta(editor: Editor): Pair<Boolean, Delta?> {
 
     return DeltaCacheService.getInstance(project)
         .get(cacheQuery)
-}
-
-fun sortDeltaFindings(
-    map: ConcurrentHashMap<String, Delta>
-): List<Map.Entry<String, Delta>> {
-    val entryList = map.entries.toList()
-    val sortingCriteria = CodeSceneGlobalSettingsStore.getInstance().state.monitorTreeSortOption
-
-    return when (sortingCriteria) {
-        MonitorTreeSortOptions.FILE_NAME -> entryList.sortedBy { it.key }
-        MonitorTreeSortOptions.SCORE_ASCENDING -> entryList.sortedByDescending { safeScoreDelta(it.value.newScore, it.value.oldScore) }
-        MonitorTreeSortOptions.SCORE_DESCENDING -> entryList.sortedBy { safeScoreDelta(it.value.newScore, it.value.oldScore) }
-    }
-}
-
-private fun safeScoreDelta(newScore: Optional<Double>, oldScore: Optional<Double>): Double {
-    val old = oldScore.orElse(null)
-    val new = newScore.orElse(null)
-
-    return if (old != null && new != null) old - new else Double.NEGATIVE_INFINITY
-}
-
-fun extractFileName(input: String): String? {
-    val regex = """<html>([^<]+)<span""".toRegex()
-
-    val matchResult = regex.find(input)
-
-    return matchResult?.groups?.get(1)?.value
 }
