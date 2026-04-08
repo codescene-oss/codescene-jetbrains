d29e3ec10b9529375cb27979249e1f38a5359210
vemvcs <victor.valenzuela+gh@codescene.com>
2026-04-08 10:55:59 +0200

    Merge pull request #118 from codescene-oss/makefile-tweaks
    chore: improve Makefile

--- .github/run-quiet.clj
+++ .github/run-quiet.clj
@@ -2,13 +2,14 @@
 (require '[babashka.process :refer [process]]
          '[babashka.fs :as fs])
 
-(when (< (count *command-line-args*) 2)
+(when (< (count *command-line-args*) 3)
   (binding [*out* *err*]
-    (println "Usage: run-quiet.clj <log-file> <program> [args...]"))
+    (println "Usage: run-quiet.clj <log-file> <description> <program> [args...]"))
   (System/exit 2))
 
 (let [log-file (first *command-line-args*)
-      prog-args (rest *command-line-args*)
+      description (second *command-line-args*)
+      prog-args (drop 2 *command-line-args*)
       p @(apply process {:out :string :err :string :continue true} prog-args)
       exit (:exit p)
       out (or (:out p) "")
@@ -19,9 +20,15 @@
   (spit log-file combined)
   (if (= 0 exit)
     (do
+      (when (seq out)
+        (let [lines (clojure.string/split-lines out)]
+          (when (seq lines)
+            (run! println (take-last 2 lines)))))
+      (println (str "command `" description "` succeeded"))
       (fs/delete-if-exists log-file)
       (System/exit 0))
     (do
       (when (seq combined) (print combined))
+      (println (str "command `" description "` failed. Logs available at " log-file))
       (flush)
       (System/exit exit))))
